fe9be34d58dd2b3af63b5335b7b4a57cc90aa81f
root <root@localhost.localdomain>
2015-04-22 19:31:44 +0800

    fix vidoe bug

--- app/jobs/video_process_job.rb
+++ app/jobs/video_process_job.rb
@@ -6,10 +6,9 @@ class VideoProcessJob < ActiveJob::Base
 
   def perform(file_path, store_path, file_extension)
     file_name = file_path.gsub("#{Rails.root}/public/videos/dynamic_film/", "")
-    video_hls_name = file_name.gsub(store_path, "").gsub(file_extension, 'm3u8')
-    video_hls_path = "#{Rails.root}/public/videos/hls"
-    FileUtils.mkdir_p File.dirname(video_hls_path + video_hls_name) unless File.exists?(video_hls_path + '/' + video_hls_name)
-    split_command = "ffmpeg -v 9 -re -i #{file_path} -acodec libmp3lame -c:v libx264 -flags -global_header -map 0 -f segment -segment_time 10 -segment_list #{video_hls_path + '/'+ video_hls_name} -segment_format mpegts #{video_hls_path + '/' + video_hls_name}%04d.ts"
+    hls_name = "#{Rails.root}/public/videos/hls/#{file_name.gsub(store_path, "").gsub(file_extension, 'm3u8')}"
+    FileUtils.mkdir_p File.dirname(hls_name) unless File.exists?(hls_name)
+    split_command = "ffmpeg -v 9 -re -i #{file_path} -acodec libmp3lame -c:v libx264 -flags -global_header -map 0 -f segment -segment_time 10 -segment_list #{hls_name} -segment_format mpegts #{hls_name}%04d.ts"
     puts split_command
     system(split_command)
   end
