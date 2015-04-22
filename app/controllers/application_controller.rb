42199822372d1ea626f9dc9a3216aa6dc92452ff
root <root@localhost.localdomain>
2015-04-22 15:39:57 +0800

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   $host = 'http://stage.e-mxing.com'
   $hls_host = 'http://video.e-mxing.com/hls'
+  $img_host = 'http://stage.e-mxing.com/images'
 end
