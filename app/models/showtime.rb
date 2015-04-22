7ec42274436b7707cbbfeb18ad85d93c62e05cd5
root <root@localhost.localdomain>
2015-04-22 16:58:14 +0800

    fix vidoe bug

--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -1,4 +1,13 @@
 class Showtime<Dynamic
+  before_create :build_film
   default_scope { where(top: 1).order(id: :desc) }
   belongs_to :user
+  attr_accessor :title
+  attr_accessor :cover
+  attr_accessor :film
+  private
+  def build_film
+    build_dynamic_film(title: title, cover: cover, film: film)
+    true
+  end
 end
