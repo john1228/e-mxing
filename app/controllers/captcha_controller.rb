0f824a9db62ebf7f5d7a16fcb5faf1b1dcdea372
root <root@localhost.localdomain>
2015-05-04 09:48:13 +0800

    change mobile binding

--- app/controllers/captcha_controller.rb
+++ app/controllers/captcha_controller.rb
@@ -2,12 +2,12 @@ class CaptchaController < ApplicationController
   include CaptchaManager
 
   def regist
-    profile = Profile.find_by(mobile: params[:mobile])
-    if profile.nil?
-      captcha = Captcha.create(mobile: params[:mobile])
-      send_sms(params[:mobile], captcha.captcha)
+    user = User.find_by(mobile: params[:mobile])
+    if user.nil?
+      captcha = generate_captcha
+      send_sms(params[:mobile], captcha)
       mobile_token = Digest::MD5.hexdigest(params[:mobile])
-      Rails.cache.write(mobile_token, {action: 'regist', mobile: params[:mobile], captcha: captcha.captcha}, expires_in: 30*60)
+      Rails.cache.write(mobile_token, {action: 'regist', mobile: params[:mobile], captcha: captcha}, expires_in: 30*60)
       render json: {
                  code: 1,
                  data: {token: mobile_token}
@@ -28,10 +28,10 @@ class CaptchaController < ApplicationController
                  message: '该号码还未注册'
              }
     else
-      captcha = Captcha.create(mobile: params[:mobile])
-      send_sms(params[:mobile], captcha.captcha)
+      captcha = generate_captcha
+      send_sms(params[:mobile], captcha)
       mobile_token = Digest::MD5.hexdigest(params[:mobile])
-      Rails.cache.write(mobile_token, {action: 'change', mobile: params[:mobile], captcha: captcha.captcha}, expires_in: 30*60)
+      Rails.cache.write(mobile_token, {action: 'change', mobile: params[:mobile], captcha: captcha}, expires_in: 30*60)
       render json: {
                  code: 1,
                  data: {token: mobile_token}
@@ -43,9 +43,9 @@ class CaptchaController < ApplicationController
   def binding
     user = User.find_by(mobile: params[:mobile])
     if user.nil?
-      captcha = Captcha.create(mobile: params[:mobile])
-      send_sms(params[:mobile], captcha.captcha)
-      Rails.cache.write("#{@user.id}_binding", {action: 'binding', mobile: params[:mobile], captcha: captcha.captcha}, expires_in: 30*60)
+      captcha = generate_captcha
+      send_sms(params[:mobile], captcha)
+      Rails.cache.write("#{@user.id}_binding", {action: 'binding', mobile: params[:mobile], captcha: captcha}, expires_in: 30*60)
       render json: {code: 1}
     else
       render json: {code: 0, message: '该号码已绑定'}
@@ -54,5 +54,11 @@ class CaptchaController < ApplicationController
 
 
   def check
+  end
+
+  private
+  def generate_captcha
+    sample_str = %w"0 1 2 3 4 5 6 7 8 9"
+    sample_str.sample(6).join('')
   end
 end
