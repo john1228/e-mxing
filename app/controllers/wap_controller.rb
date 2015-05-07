1e76368ae484d8b9dfc58cd2c214a7a146173571
root <root@localhost.localdomain>
2015-05-07 16:47:56 +0800

    film share

--- app/controllers/wap_controller.rb
+++ app/controllers/wap_controller.rb
@@ -5,14 +5,7 @@ class WapController < ApplicationController
   end
 
   def film
-    @film = Film.find_by(id: params[:id])
-    @actor = @film.dynamic.user.profile
-  end
-
-  def course
-    @course = Course.find_by(id: params[:id])
-    @actor = Profile.find_by(user_id: @course.user_id)
-    latest_schedule = Schedule.where(course_id: @course.id).last
-    @latester = latest_schedule.nil? ? nil : Profile.find_by(user_id: latest_schedule.user_id)
+    @dynamic= Dynamic.find_by(id: params[:id])
+    @actor = @dynamic.user.profile
   end
 end
