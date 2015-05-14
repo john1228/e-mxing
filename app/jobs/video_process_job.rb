b276b3b9d3991ce8a88351401c43d59d71490bf0
root <root@localhost.localdomain>
2015-05-14 16:19:10 +0800

    video change

--- app/jobs/video_process_job.rb
+++ app/jobs/video_process_job.rb
@@ -1,11 +1,11 @@
 class VideoProcessJob < ActiveJob::Base
   queue_as :default
-  
+
   def perform(file_path, store_path, file_extension)
     file_name = file_path.gsub("#{Rails.root}/public/videos/dynamic_film/", '')
 
     transcode_path = "#{file_path.gsub(".#{file_extension}", '')}_1.mp4"
-    transcode_command = "ffmpeg -i #{file_path}  -vcodec libx264 -maxrate 500k -bufsize 1000k -vf scale=640:480 -threads 0 -f mp4 #{transcode_path}"
+    transcode_command = "ffmpeg -i #{file_path}  -vcodec libx264 -maxrate 500k -bufsize 1000k -vf scale=640:-1 -threads 0 -f mp4 #{transcode_path}"
     system(transcode_command)
 
     hls_name = "#{Rails.root}/public/videos/hls/#{file_name.gsub(store_path, "").gsub(file_extension, 'm3u8')}"
