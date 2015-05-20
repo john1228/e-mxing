0ec29e2564cdbc73d006b091b602e4178a007185
root <root@localhost.localdomain>
2015-05-20 17:51:24 +0800

    fix hls bug

--- app/uploaders/film_uploader.rb
+++ app/uploaders/film_uploader.rb
@@ -10,7 +10,7 @@ class FilmUploader < CarrierWave::Uploader::Base
   end
 
   def hls
-    $hls_host + url.gsub("/#{store_dir}", '').gsub(file.extension, 'm3u8')
+    $hls_host + file.path.gsub("#{Rails.root}/public/#{store_dir}", '').gsub(file.extension, 'm3u8')
   end
 
 
