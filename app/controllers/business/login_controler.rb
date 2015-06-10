870ef849e7c221023f31be0c7e3ef7f3093de51e
root <root@localhost.localdomain>
2015-06-10 12:38:04 +0800

    20150610

--- app/controllers/business/login_controler.rb
+++ app/controllers/business/login_controler.rb
@@ -15,7 +15,7 @@ module Business
       if coach.present?
         submit_password = Digest::MD5.hexdigest("#{params[:password]}|#{coach.salt}")
         if coach.password.eql?(submit_password)
-          Rails.cache.write(coach.token, coach)
+          Rails.cache.write("#{coach.token}|gyms", coach)
           render json: Success.new({coach: coach.summary_json})
         else
           render json: Failure.new('您输入的密码不正确')
