e449ded69b33686fe0a596e724572a2822f86ded
root <root@localhost.localdomain>
2015-06-16 17:12:58 +0800

    wallet

--- app/controllers/business/wallet_controller.rb
+++ app/controllers/business/wallet_controller.rb
@@ -8,5 +8,16 @@ module Business
     def coupons
       render json: Success.new(coupons: Coupon.where(id: @user.wallet.coupons.split(',')))
     end
+
+    def detail
+      render json: Success.new(detail: @coach.wallet.wallet_logs.where.not(balance: 0).page(params[:page]||1).map { |log|
+                                 {
+                                     id: log.id,
+                                     action: log.action,
+                                     balance: log.balance,
+                                     created: created_at.to_i
+                                 }
+                               })
+    end
   end
 end
