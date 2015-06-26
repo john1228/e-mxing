8d8a44ad1e1b50d2d494e8212838f571d4411e6b
root <root@localhost.localdomain>
2015-06-26 17:16:07 +0800

    change appoinment

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,11 @@
 class Like < ActiveRecord::Base
-  before_save :validate_within_month
+  before_save :within_month
   DYNAMIC = 1
   PERSON = 2
 
   private
-  def validate_within_month
-    liked = where(created_at: Time.now.at_beginning_of_month..Time.now).take
+  def within_month
+    liked = Like.where(created_at: Time.now.at_beginning_of_month..Time.now).take
     return false if liked.present?
   end
 end
