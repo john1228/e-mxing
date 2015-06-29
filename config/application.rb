407868b543be3d5410cd6cb75e0978cfd3f6a424
root <root@localhost.localdomain>
2015-06-29 17:19:26 +0800

    counter cache

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Mxing
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.insert_after ActionDispatch::ParamsParser, ActionDispatch::XmlParamsParser
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
