993fc597b0de01acfefc35d02949ba29492ae846
root <root@localhost.localdomain>
2015-06-30 12:29:27 +0800

    afa

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,6 +3,7 @@ class NewsController < ApplicationController
     @news = News.find_by(id: params[:id])
     @cc = params[:cc]
     @created = @news.created_at.localtime.strftime('%m-%d %H:%M')
+    logger.info "新闻时间:#{@created}"
     render layout: 'news'
   end
 end
