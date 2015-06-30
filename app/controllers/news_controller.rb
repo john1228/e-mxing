fbeb66ea2e523de51519b46a3d9b5e1e069017a7
root <root@localhost.localdomain>
2015-06-30 12:25:28 +0800

    afa

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,6 +2,7 @@ class NewsController < ApplicationController
   def show
     @news = News.find_by(id: params[:id])
     @cc = params[:cc]
+    @created = @news.created_at.localtime.strftime('%m-%d %H:%M')
     render layout: 'news'
   end
 end
--- app/controllers/type_shows_controller.rb
+++ app/controllers/type_shows_controller.rb
@@ -2,6 +2,7 @@ class TypeShowsController < ApplicationController
   def show
     @show = TypeShow.find_by(id: params[:id])
     @cc = params[:cc]
+    @created = @show.created_at.localtime.strftime('%m-%d %H:%M')
     render layout: 'type_show'
   end
 end
