339a485124e229864175ad06394d1e31a84a60dd
root <root@localhost.localdomain>
2015-07-01 20:02:55 +0800

    share

--- app/controllers/business/appointments_controller.rb
+++ app/controllers/business/appointments_controller.rb
@@ -12,24 +12,24 @@ module Business
     end
 
     def create
-      begin
-        user = User.find_by_mxid(params[:user])
-        lesson = user.lessons.find_by(id: params[:lesson], coach: @coach)
-        remain = lesson.available - lesson.used
-        amount = params[:amount].to_i
-        if remain>= amount
-          appointment = Appointment.new(user: user, coach: @coach, lesson: lesson, course: lesson.course, amount: params[:amount])
-          if appointment.save
-            render json: Success.new
-          else
-            render json: Failure.new(appointment.errors.map { |k, v| "#{k}:#{v}" })
-          end
+      #begin
+      user = User.find_by_mxid(params[:user])
+      lesson = user.lessons.find_by(id: params[:lesson], coach: @coach)
+      remain = lesson.available - lesson.used
+      amount = params[:amount].to_i
+      if remain>= amount
+        appointment = Appointment.new(user: user, coach: @coach, lesson: lesson, course: lesson.course, amount: params[:amount])
+        if appointment.save
+          render json: Success.new
         else
-          render json: Failure.new('该学员剩余到课程不足')
+          render json: Failure.new(appointment.errors.map { |k, v| "#{k}:#{v}" })
         end
-      rescue Exception => exp
-        render json: Failure.new(exp.message)
+      else
+        render json: Failure.new('该学员剩余到课程不足')
       end
+      # rescue Exception => exp
+      #   render json: Failure.new(exp.message)
+      # end
     end
   end
 end
