8163bf653e816c3abcf21ca3a844baf2f8951879
root <root@localhost.localdomain>
2015-07-02 11:21:16 +0800

    update

--- app/controllers/callback/alipay_controller.rb
+++ app/controllers/callback/alipay_controller.rb
@@ -6,14 +6,17 @@ module Callback
       buyer_id = params[:buyer_id]
       buyer_email = params[:buyer_email]
       pay_amount = params[:price]
-      order = Order.find_by(no: order_no)
-      order.update(status: Order::STATUS[:pay], pay_type: Order::PAY_TYPE[:alipay]) #if order.pay_amount.eql?(pay_amount.to_d)
-      Transaction.create(no: trade_no, order_no: order_no, buyer_id: buyer_id, buyer_email: buyer_email, source: Transaction::SOURCE[:alipay], price: pay_amount)
+      transaction = Transaction.find_by(no: trade_no, source: Transaction::SOURCE[:alipay])
+      if transaction.blank?
+        order = Order.find_by(no: order_no)
+        order.update(status: Order::STATUS[:pay], pay_type: Order::PAY_TYPE[:alipay]) if order.pay_amount.eql?(pay_amount.to_d)&&order.status.eql?(Order::STATUS[:pay])
+        Transaction.create(no: trade_no, order_no: order_no, buyer_id: buyer_id, buyer_email: buyer_email, source: Transaction::SOURCE[:alipay], price: pay_amount)
 
-      course = order.order_items.first.course
-      unless course.guarantee.eql?(Course::GUARANTEE)
-        coach = course.coach
-        coach.wallet.update(balance: (coach.wallet.balance+BigDecimal(pay_amount)), action: WalletLog::ACTIONS['消费'])
+        course = order.order_items.first.course
+        unless course.guarantee.eql?(Course::GUARANTEE)
+          coach = course.coach
+          coach.wallet.update(balance: (coach.wallet.balance+BigDecimal(pay_amount)), action: WalletLog::ACTIONS['消费'])
+        end
       end
       render text: 'success'
     end
--- app/controllers/callback/webchat_controller.rb
+++ app/controllers/callback/webchat_controller.rb
@@ -6,15 +6,19 @@ module Callback
       order_no = request_data[:out_trade_no]
       buyer_id = request_data[:openid]
       pay_amount = (request_data[:total_fee].to_i/100.to_f)
-      order = Order.find_by(no: order_no)
-      order.update(status: Order::STATUS[:pay], pay_type: Order::PAY_TYPE[:webchat]) #if order.pay_amount.eql?(pay_amount.to_d)
-      Transaction.create(no: trade_no, order_no: order_no, buyer_id: buyer_id, source: Transaction::SOURCE[:webchat], price: pay_amount)
+      transaction = Transaction.find_by(no: trade_no, source: Transaction::SOURCE[:webchat])
+      if transaction.blank?
+        order = Order.find_by(no: order_no)
+        order.update(status: Order::STATUS[:pay], pay_type: Order::PAY_TYPE[:alipay]) if order.pay_amount.eql?(pay_amount.to_d)&&order.status.eql?(Order::STATUS[:pay])
+        Transaction.create(no: trade_no, order_no: order_no, buyer_id: buyer_id, source: Transaction::SOURCE[:webchat], price: pay_amount)
 
-      course = order.order_items.first.course
-      unless course.guarantee.eql?(Course::GUARANTEE)
-        coach = course.coach
-        coach.wallet.update(balance: (coach.wallet.balance+BigDecimal(pay_amount.tof)), action: WalletLog::ACTIONS['消费'])
+        course = order.order_items.first.course
+        unless course.guarantee.eql?(Course::GUARANTEE)
+          coach = course.coach
+          coach.wallet.update(balance: (coach.wallet.balance+BigDecimal(pay_amount.tof)), action: WalletLog::ACTIONS['消费'])
+        end
       end
+
       render text: 'success'
     end
   end
