1d1d63c56cc1db1ee6b3399ad71ed5f9e8157b37
root <root@localhost.localdomain>
2015-07-02 13:47:07 +0800

    update

--- db/migrate/20150629021326_create_settings.rb
+++ db/migrate/20150629021326_create_settings.rb
@@ -4,7 +4,5 @@ class CreateSettings < ActiveRecord::Migration
       t.references :user
       t.integer :stealth #隐身设置
     end
-    remove_column :courses, :order_items_count
-    add_column :courses, :order_items_count, :integer, default: 0
   end
 end
