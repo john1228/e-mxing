7420d9784787269e6e4dd41bdbba67d04455be75
root <root@localhost.localdomain>
2015-07-03 11:18:18 +0800

    update

--- app/models/like.rb
+++ app/models/like.rb
@@ -6,7 +6,7 @@ class Like < ActiveRecord::Base
 
   private
   def within_month
-    liked = Like.where(user_id: user_id, created_at: Time.now.at_beginning_of_month..Time.now).take
+    liked = Like.where(user_id: user_id, liked_id: liked_id, like_type: PERSON, created_at: Time.now.at_beginning_of_month..Time.now).take
     return false if liked.present?
   end
 end
