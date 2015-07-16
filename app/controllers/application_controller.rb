496b3990aaeafa819870df542885508d4d2d484e
root <root@localhost.localdomain>
2015-07-16 10:21:58 +0800

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
   $img_host = 'http://stage.e-mxing.com/images'
 
   def access_denied(exception)
+    redirect_to new_admin_user_session_path, :alert => exception.message
   end
 end
