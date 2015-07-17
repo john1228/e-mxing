64c012857972d8f6cd019c8e82889d26ce61cf5d
root <root@localhost.localdomain>
2015-07-17 17:32:15 +0800

    events

--- app/admin/gallery.rb
+++ app/admin/gallery.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Gallery do
   menu label: '图库'
-  permit_params :tag, images_attributes: [:id, :image]
+  permit_params :tag, images_attributes: [:id, :image, :caption]
   form partial: 'form'
 end
