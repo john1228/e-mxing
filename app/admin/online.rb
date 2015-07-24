5b1418d15c4c51bfb0d5efaca8e6d95fc9deb744
root <root@localhost.localdomain>
2015-07-24 10:27:53 +0800

    gallery

--- app/admin/online.rb
+++ app/admin/online.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Online do
-  menu label: '在线',parent: '原始数据'
+  menu label: '在线', parent: '原始数据'
   index do
     column('设备', :device)
     column('打开时间') { |online| online.open.strftime('%Y-%m-%d %H:%M:%S') }
--- app/admin/order.rb
+++ app/admin/order.rb
@@ -14,8 +14,28 @@ ActiveAdmin.register Order do
     column('联系电话', :contact_phone)
     column('订单金额', :total)
     column('支付金额', :pay_amount)
-    column('支付方式', :pay_type)
-    column('状态', :status)
+    column('支付方式') { |order|
+      case order.pay_type
+        when 1
+          '支付宝'
+        when 2
+          '微信'
+        when 3
+          '京东'
+      end
+    }
+    column('状态') { |order|
+      case order.status
+        when -1
+          status_tag('已删除', :error)
+        when 0
+          status_tag('已取消', :error)
+        when 1
+          status_tag('未付款', :warn)
+        when 2
+          status_tag('已付款', :ok)
+      end
+    }
     column('时间') { |order| order.updated_at.strftime('%Y-%m-%d %H:%M:%S') }
   end
 end
