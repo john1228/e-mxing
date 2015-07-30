8915d2e792689afb83ad0c3bc14d2328b9b6c2a2
root <root@localhost.localdomain>
2015-07-30 14:53:33 +0800

    V3

--- db/migrate/20150730064558_migrate_course.rb
+++ db/migrate/20150730064558_migrate_course.rb
@@ -2,15 +2,14 @@ class MigrateCourse < ActiveRecord::Migration
   def change
     Course.all { |course|
       service = course.coach.service
-      unless service.blank?
-        Sku.create(
-            sku: 'CC'+'-' + '%06d' % course.id + '-' + '%06d' % (service.id),
-            market_price: course.price,
-            selling_price: course.price,
-            address: service.address||'',
-            coordinate: (service.place.lonlat rescue 'POINT(0 0)')
-        )
-      end
+      puts service.present?
+      Sku.create(
+          sku: 'CC'+'-' + '%06d' % course.id + '-' + '%06d' % (service.id),
+          market_price: course.price,
+          selling_price: course.price,
+          address: service.address||'',
+          coordinate: (service.place.lonlat rescue 'POINT(0 0)')
+      ) if service.present?
     }
   end
 end
