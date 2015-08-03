e49953c0c84c47c195dd3d74ebea615b28d56288
root <root@localhost.localdomain>
2015-08-03 14:19:43 +0800

    V3

--- db/migrate/20150803060531_change_comment_table.rb
+++ db/migrate/20150803060531_change_comment_table.rb
@@ -3,5 +3,9 @@ class ChangeCommentTable < ActiveRecord::Migration
     remove_column :comments, :prof
     remove_column :comments, :punc
     remove_column :comments, :space
+    remove_column :comments, :comm
+    remove_column :comments, :course_id
+    add_column :comments, :score, :integer
+    add_column :comments, :sku, :string
   end
 end
