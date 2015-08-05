0c222b5df0db18aae494a7d19a155a17c9545260
root <root@localhost.localdomain>
2015-08-05 15:35:03 +0800

    V3

--- app/controllers/gyms/courses_controller.rb
+++ app/controllers/gyms/courses_controller.rb
@@ -2,7 +2,7 @@ module Gyms
   class CoursesController < BaseController
     def index
       render json: Success.new(
-                 courses: Sku.where(seller_id: @coach.id).where('sku LIKE "CC%"').order(id: :desc).page(params[:page]||1)
+                 courses: Sku.where(seller_id: @coach.id).where("sku LIKE 'CC%'").order(id: :desc).page(params[:page]||1)
              )
     end
   end
