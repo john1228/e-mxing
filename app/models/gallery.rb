bad30dc7096caaf0ac1c2893be31cb7d0d673bd8
root <root@localhost.localdomain>
2015-08-05 10:46:29 +0800

    V3

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,9 +1,9 @@
 class Gallery < ActiveRecord::Base
-  TAGS = %w'热门图片 廋成闪电 廋成闪电1 廋成闪电2 廋成闪电3 廋成闪电4 廋成闪电5 廋成闪电6 廋成闪电7 廋成闪电8'
+  TAGS = %w'食之有胃 腰炫腹 汗水日记 小萌妹大美腿 性感电臀 万瘦无疆 波涛胸涌 男神女神 硬成狗 大块头大智慧'
   has_many :images, class_name: GalleryImage, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   validates_presence_of :tag, :images
-  validates_length_of :images, maximum: 6,message: '最多只能上传6张图片'
+  validates_length_of :images, maximum: 6, message: '最多只能上传6张图片'
 
   def as_json
     images.map { |image|
