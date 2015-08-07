17abd548ef208b9945790cc5177bd4e15c870e46
root <root@localhost.localdomain>
2015-08-07 17:31:01 +0800

    V3

--- app/admin/service_course.rb
+++ app/admin/service_course.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register ServiceCourse do
 
   permit_params :name, :type, :style, :during, :limit_start, :limit_end, :proposal, :exp, :intro, :special, :market_price, :selling_price, :store,
                 :agency, :limit, service: [], image: []
-
+  actions :index, :show
   scope('0-全部', :all, default: true)
   scope('1-在售', :online) { |scope| scope.where(status: ServiceCourse::STATUS[:online]) }
   scope('2-仓库', :offline) { |scope| scope.where(status: ServiceCourse::STATUS[:offline]) }
@@ -12,12 +12,12 @@ ActiveAdmin.register ServiceCourse do
     selectable_column
     column(:name)
     column(:type)
-    column(:name)
     column(:during)
     column(:proposal)
     column(:exp)
     actions do |course|
-      link_to '上架', online_path(course), method: :post
+      link_to '上架', online_path(course), method: :post unless course.status.eql?(0)
+      link_to '下架', '', method: :post unless course.status.eql?(1)
     end
   end
 
@@ -31,6 +31,10 @@ ActiveAdmin.register ServiceCourse do
   show do
     attributes_table do
       row :name
+      row :type
+      row :during
+      row :proposal
+      row :exp
     end
   end
 
