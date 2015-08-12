738d6bace5f2662e31cbae1cb03280270dc02b82
root <root@localhost.localdomain>
2015-08-12 22:22:19 +0800

    V3

--- app/jobs/message_job.rb
+++ app/jobs/message_job.rb
@@ -2,13 +2,13 @@ class MessageJob < ActiveJob::Base
   queue_as :default
 
   def perform(user_id, message)
-    user = User.find_by(id: user_id)
+    profile = Profile.find_by(id: user_id)
     access_token = Rails.cache.fetch('mob')
     Faraday.post do |req|
       req.url "#{MOB['host']}/messages"
       req.headers['Content-Type'] = 'application/json'
       req.headers['Authorization'] = "Bearer #{access_token}"
-      req.body = {target_type: 'users', target: ["#{user.profile.mxid}"], msg: {type: 'txt', msg: message}}.to_json.to_s
+      req.body = {target_type: 'users', target: ["#{profile.mxid}"], msg: {type: 'txt', msg: message}}.to_json.to_s
     end
   end
 end
