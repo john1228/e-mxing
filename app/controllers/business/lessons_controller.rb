e14b55d79783571b129e0c10b9505bf06a10ab23
root <root@localhost.localdomain>
2015-08-12 10:58:06 +0800

    B

--- app/controllers/business/lessons_controller.rb
+++ app/controllers/business/lessons_controller.rb
@@ -16,22 +16,9 @@ module Business
     def update
       begin
         lesson = Lesson.where('? = ANY (code)', params[:code]).take
-        render json: Failure.new('消课失败:错误的消课码') if lesson.present?
-        sku = Sku.find_by(sku: lesson.sku)
-        if sku.seller_id.eql?(@coach.id)
-          appointment = @coach.appointments.new(
-              lesson_id: lesson.id, user_id: lesson.user_id,
-              sku: lesson.sku, code: params[:code], amount: 1,
-              status: Appointment::STATUS[:confirm]
-          )
-          if appointment.save
-            render json: Success.new
-          else
-            render json: Failure.new('消课失败')
-          end
-        else
-          service = Service.find_by(id: sku.seller_id)
-          if service.present? && service.coaches.include?(@coach)
+        if lesson.present?
+          sku = Sku.find_by(sku: lesson.sku)
+          if sku.seller_id.eql?(@coach.id)
             appointment = @coach.appointments.new(
                 lesson_id: lesson.id, user_id: lesson.user_id,
                 sku: lesson.sku, code: params[:code], amount: 1,
@@ -43,8 +30,24 @@ module Business
               render json: Failure.new('消课失败')
             end
           else
-            render json: Failure.new('您没有权限消除该课时')
+            service = Service.find_by(id: sku.seller_id)
+            if service.present? && service.coaches.include?(@coach)
+              appointment = @coach.appointments.new(
+                  lesson_id: lesson.id, user_id: lesson.user_id,
+                  sku: lesson.sku, code: params[:code], amount: 1,
+                  status: Appointment::STATUS[:confirm]
+              )
+              if appointment.save
+                render json: Success.new
+              else
+                render json: Failure.new('消课失败')
+              end
+            else
+              render json: Failure.new('您没有权限消除该课时')
+            end
           end
+        else
+          render json: Failure.new('无效到课程码')
         end
       rescue Exception => exp
         render json: Failure.new('消课失败')
