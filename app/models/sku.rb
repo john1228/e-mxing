e653183b05b99d5e412cc2809e75a8a85d343895
root <root@localhost.localdomain>
2015-08-13 17:50:38 +0800

    V3

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -100,7 +100,7 @@ class Sku < ActiveRecord::Base
   end
 
   def buyers
-    User.where(id: Order.includes(:order_item).where('order_items.sku LIKE ?', sku[0, sku.rindex('-')] + '%').order(id: :desc).pluck(:user_id)).limit(5).map { |user|
+    User.where(id: Order.includes(:order_item).where('orders.status = ? and order_items.sku LIKE ?', Order::STATUS[:pay], sku[0, sku.rindex('-')] + '%').order(id: :desc).pluck(:user_id)).limit(5).map { |user|
       {
           mxid: user.profile.mxid,
           name: user.profile.name,
