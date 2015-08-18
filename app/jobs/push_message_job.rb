77485f2831455df3a840becc9cdca374c4d77358
root <root@localhost.localdomain>
2015-08-18 14:14:56 +0800

    add

--- app/jobs/push_message_job.rb
+++ app/jobs/push_message_job.rb
@@ -2,20 +2,15 @@ class PushMessageJob < ActiveJob::Base
   queue_as :default
 
   def perform(target, message)
-    token_response = Faraday.post do |req|
-      req.url 'https://a1.easemob.com/jsnetwork/mxing/token'
-      req.headers['Content-Type'] = 'application/json'
-      req.body = "{\"grant_type\": \"client_credentials\", \"client_id\": \"YXA6NQmy0PIkEeSQO18Yeq100Q\", \"client_secret\": \"YXA6t1SdtNrJAAHq6m3Bu3Yx1Ryr8jI\"}"
-    end
-    easemob_body = JSON.parse(token_response.body)
-    easemob_token = easemob_body['access_token']
+    access_token = Rails.cache.fetch('mob')
     (0..(target.size/20)).map { |index|
-      Faraday.post do |req|
+      resp = Faraday.post do |req|
         req.url 'https://a1.easemob.com/jsnetwork/mxingsijiao/messages'
         req.headers['Content-Type'] = 'application/json'
-        req.headers['Authorization'] = "Bearer #{easemob_token}"
+        req.headers['Authorization'] = "Bearer #{access_token}"
         req.body = {target_type: 'users', target: target[index*20, 20-1], msg: {type: 'txt', msg: message}}.to_json.to_s
       end
+      puts resp.body
       sleep(0.1)
     }
   end
