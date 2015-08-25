d6016722a86f0b6679a8fc9cc58bf72f072ffb51
root <root@localhost.localdomain>
2015-08-25 17:23:38 +0800

    friends

--- app/controllers/agency/courses_controller.rb
+++ app/controllers/agency/courses_controller.rb
@@ -2,7 +2,11 @@ module Agency
   class CoursesController < BaseController
     def index
       render json: Success.new(
-                 courses: Sku.online.where(seller_id: @agency.id).where("sku LIKE 'SC%'").order(id: :desc).page(params[:page]||1)
+                 courses: Sku.online.
+                     select("skus.*, st_distance(skus.coordinate, 'POINT(#{params[:lng]} #{params[:lat]})') as distance").
+                     where(seller_id: @agency.id).
+                     where("sku LIKE 'SC%'").
+                     order(id: :desc).page(params[:page]||1)
              )
     end
   end
