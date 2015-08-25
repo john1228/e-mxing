55942ed111d3303d216f8d6e7ec55bbe7cf1021e
root <root@localhost.localdomain>
2015-08-25 16:43:25 +0800

    friends

--- app/controllers/agency/courses_controller.rb
+++ app/controllers/agency/courses_controller.rb
@@ -2,7 +2,7 @@ module Agency
   class CoursesController < BaseController
     def index
       render json: Success.new(
-                 courses: Sku.where(seller_id: @agency.id).where("sku LIKE 'SC%'").order(id: :desc).page(params[:page]||1)
+                 courses: Sku.online.where(seller_id: @agency.id).where("sku LIKE 'SC%'").order(id: :desc).page(params[:page]||1)
              )
     end
   end
