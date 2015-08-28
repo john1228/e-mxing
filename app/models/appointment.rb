ef134f3d5ff0fa4686775968d94a36ae2264babb
root <root@localhost.localdomain>
2015-08-28 22:57:43 +0800

    images

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -33,7 +33,11 @@ class Appointment < ActiveRecord::Base
           service = coach.service
           wallet = Wallet.find_or_create_by(user: service)
           #购买时课程单价
-          wallet.update(balance: (wallet.balance + course.price*amount), action: WalletLog::ACTIONS['卖课收入'])
+          wallet.with_lock do
+            wallet.balance += (sku_course.selling_price * amount)
+            wallet.action = WalletLog::ACTIONS['卖课收入']
+            wallet.save
+          end
         end
         MessageJob.perform_later(user.id, MESSAGE['消课'] % [sku_course.course_name, created_at.strftime('%Y%m%d')+'%05d' % id])
         SmsJob.perform_later(lesson.order.contact_phone, SMS['消课'], [sku_course.course_name, created_at.strftime('%Y%m%d')+'%05d' % id])
