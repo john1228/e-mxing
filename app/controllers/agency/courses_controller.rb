bfe0bae2d5d842f041e3697e15000b5f97a43751
root <root@localhost.localdomain>
2015-08-28 14:39:08 +0800

    distanc

--- app/controllers/agency/courses_controller.rb
+++ app/controllers/agency/courses_controller.rb
@@ -3,7 +3,7 @@ module Agency
     def index
       render json: Success.new(
                  courses: Sku.online.
-                     select("skus.*, st_distance(skus.coordinate, 'POINT(#{params[:lng]} #{params[:lat]})') as distance").
+                     select("skus.*, st_distance(skus.coordinate, 'POINT(#{params[:lng]||0} #{params[:lat]||0})') as distance").
                      where(seller_id: @agency.id).
                      where("sku LIKE 'SC%'").
                      order(id: :desc).page(params[:page]||1)
--- app/controllers/gyms/courses_controller.rb
+++ app/controllers/gyms/courses_controller.rb
@@ -4,7 +4,7 @@ module Gyms
       if params[:version].present?
         render json: Success.new(
                    courses: Sku.online.
-                       select("skus.*, st_distance(skus.coordinate, 'POINT(#{params[:lng]} #{params[:lat]})') as distance").
+                       select("skus.*, st_distance(skus.coordinate, 'POINT(#{params[:lng]||0} #{params[:lat]||0})') as distance").
                        where(seller_id: @coach.id).
                        where("sku LIKE 'CC%'").
                        order(id: :desc).page(params[:page]||1)
