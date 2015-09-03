698555335c53c5fa4e320c0196c904bd5b41afc2
john1228 <313423716@qq.com>
2015-09-03 19:11:21 +0800

    Create news_controller.rb

---
+++ app/controllers/find/news_controller.rb
@@ -0,0 +1,19 @@
+module Find
+  class NewsController < ApplicationController
+    def index
+      if params[:tag].blank?
+        render json: Success.new(news: [{
+                                            tag: tag,
+                                            item: News.where(tag: tag).order(id: :desc).page(params[:page]||1)
+                                        }])
+      else
+        render json: Success.new(news: News::TAGS.map { |tag|
+                                   {
+                                       tag: tag,
+                                       item: News.where(tag: tag).order(id: :desc).take(2)
+                                   }
+                                 })
+      end
+    end
+  end
+end
