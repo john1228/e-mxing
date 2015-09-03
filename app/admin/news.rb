c29def0220159970a088c152000ec45eb3f24940
john1228 <313423716@qq.com>
2015-09-03 18:32:35 +0800

    Update news.rb

--- app/admin/news.rb
+++ app/admin/news.rb
@@ -1,12 +1,15 @@
 ActiveAdmin.register News do
   menu label: '新闻', parent: '运营'
   filter :title, label: '标题'
-  permit_params :id, :title, :cover, :content
+  permit_params :id,:tag, :title, :cover, :content
 
   index title: '新闻列表' do
     selectable_column
     column '标题', :title do |news|
       truncate(news.title)
+    end
+    column '分类', :title do |news|
+      news.tag
     end
     column '封面', :title do |news|
       image_tag(news.cover.thumb.url, width: 69)
@@ -21,6 +24,7 @@ ActiveAdmin.register News do
     panel '' do
       attributes_table_for news do
         row('标题') { news.title }
+        row('分类') { news.tag }
         row('封面') { image_tag(news.cover.thumb.url, width: 69*2) }
         row('链接') { link_to('详情', news_detail_path(news)) }
       end
