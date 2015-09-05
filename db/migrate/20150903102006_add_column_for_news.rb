82a50affb3f8d4ce2f5c4a1ff10839f746d7f3ae
john1228 <313423716@qq.com>
2015-09-05 08:26:37 +0800

    Update 20150903102006_add_column_for_news.rb

--- db/migrate/20150903102006_add_column_for_news.rb
+++ db/migrate/20150903102006_add_column_for_news.rb
@@ -1,6 +1,6 @@
 class AddColumnForNews < ActiveRecord::Migration
   def change
     add_column :news, :tag, :string, default: ''
-    add_column :dynamic_images, :string,array: true, default: []
+    add_column :dynamic_images,:string, array: true, default: []
   end
 end
