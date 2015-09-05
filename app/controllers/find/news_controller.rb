9f5af99cddb7b7e08bfe071d6b8e149ab9e74055
john1228 <313423716@qq.com>
2015-09-05 22:39:18 +0800

    Update news_controller.rb

--- app/controllers/find/news_controller.rb
+++ app/controllers/find/news_controller.rb
@@ -4,13 +4,25 @@ module Find
       if params[:tag].present?
         render json: Success.new(news: [{
                                             tag: params[:tag],
-                                            item: News.where(tag: tag).order(id: :desc).page(params[:page]||1)
+                                            item: News.where(tag: tag).order(id: :desc).page(params[:page]||1).map{|news|
+                                              {
+                                                title: news.title,
+                                                cover: news.cover.url,
+                                                content: news.content[4,new.content.index('</p>')]
+                                              }
+                                            }
                                         }])
       else
         render json: Success.new(news: News::TAG.map { |tag|
                                    {
                                        tag: tag,
-                                       item: News.where(tag: tag).order(id: :desc).take(2)
+                                       item: News.where(tag: tag).order(id: :desc).take(2).map{|news|
+                                          {
+                                            title: news.title,
+                                            cover: news.cover.url,
+                                            content: news.content[4,new.content.index('</p>')]
+                                          }
+                                       }
                                    }
                                  })
       end
