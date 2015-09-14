8de4a23e5b4867f09f7352c88ad0714335c5620c
root <root@localhost.localdomain>
2015-09-14 10:20:10 +0800

    3.2.1

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,5 @@
 class News < ActiveRecord::Base
-  TAG = ['训练课', '饮食控', '私教', '机构']
+  TAG = %w'训练课 饮食控 私教 机构'
   mount_uploader :cover, NewsCoverUploader
 
 
@@ -9,7 +9,7 @@ class News < ActiveRecord::Base
         cover: cover.thumb.url,
         width: cover_width,
         height: cover_height,
-        content: content[4, content.index('</p>')],
+        content: content[0, 200],
         url: $host + "/news/#{id}"
     }
   end
