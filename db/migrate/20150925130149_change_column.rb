9b3e08e631dda13a57c69a5b355c53501734c568
root <root@localhost.localdomain>
2015-09-25 21:13:20 +0800

    3.2.2

--- db/migrate/20150925130149_change_column.rb
+++ db/migrate/20150925130149_change_column.rb
@@ -2,8 +2,6 @@ class ChangeColumn < ActiveRecord::Migration
   def change
     add_index :profiles, :name
     add_index :profiles, :address
-
-    add_index :dynamic_comments, :dynamic_id
     add_index :skus, :seller_id
     add_index :profiles, :user_id
     add_index :service_members, :service_id
