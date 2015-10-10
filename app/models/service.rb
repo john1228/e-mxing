dc2e1b4be5c182a93d14ace383c9ecd892f0d03e
root <root@localhost.localdomain>
2015-10-10 12:28:18 +0800

    agency

--- app/models/service.rb
+++ app/models/service.rb
@@ -12,7 +12,8 @@ class Service<User
   def as_json
     in_the_sale = Sku.online.where(seller_id: coaches.pluck(:id)<<id)
     top_sellers = in_the_sale.where('skus.sku LIKE ?', 'CC%').order(orders_count: :desc).order(id: :asc).pluck(:seller_id).uniq[0, 3]
-    tops = coaches.where(id: top_sellers)||coaches.order(id: :desc).take(3)
+    tops = coaches.where(id: top_sellers)
+    tops << coaches.order(id: :desc).take(3-tops.length) unless tops.length.eql?(3)
     {
         mxid: profile.mxid,
         name: profile.name,
