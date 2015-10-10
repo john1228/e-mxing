709bb4b69a074eb26954185ff7965d6445e86e65
root <root@localhost.localdomain>
2015-10-10 10:12:47 +0800

    share

--- app/controllers/discovery/images_controller.rb
+++ app/controllers/discovery/images_controller.rb
@@ -2,6 +2,12 @@ module Discovery
   class ImagesController < ApplicationController
     def index
       if params[:tag].present?
+        data = [{
+                    tag: params[:tag],
+                    item: Dynamic.joins(:dynamic_images).where('? = ANY (dynamic_images.tag)', params[:tag]).uniq.order(id: :desc).page(params[:page]||1).map { |item|
+                      item.as_json.merge(user: item.user.profile.summary_json)
+                    }
+                }]
         render json: Success.new(news: [{
                                             tag: params[:tag],
                                             item: Dynamic.joins(:dynamic_images).where('? = ANY (dynamic_images.tag)', params[:tag]).uniq.order(id: :desc).page(params[:page]||1).map { |item|
@@ -9,15 +15,17 @@ module Discovery
                                             }
                                         }])
       else
-        render json: Success.new(news: TAGS.map { |tag|
-                                   {
-                                       tag: tag,
-                                       item: Dynamic.joins(:dynamic_images).where('? = ANY (dynamic_images.tag)', tag).uniq.order(id: :desc).take(3).map { |item|
-                                         item.as_json.merge(user: item.user.profile.summary_json)
-                                       }
-                                   }
-                                 })
+        data = TAGS.map { |tag|
+          {
+              tag: tag,
+              item: Dynamic.joins(:dynamic_images).where('? = ANY (dynamic_images.tag)', tag).uniq.order(id: :desc).take(3).map { |item|
+                item.as_json.merge(user: item.user.profile.summary_json)
+              }
+          } if Dynamic.joins(:dynamic_images).where('? = ANY (dynamic_images.tag)', tag).present?
+        }
       end
+      data.delete(nil)
+      render json: Success.new(news: data)
     end
   end
 end
--- app/controllers/discovery/news_controller.rb
+++ app/controllers/discovery/news_controller.rb
@@ -2,18 +2,20 @@ module Discovery
   class NewsController < ApplicationController
     def index
       if params[:tag].present?
-        render json: Success.new(news: [{
-                                            tag: params[:tag],
-                                            item: News.where(tag: params[:tag]).order(id: :desc).page(params[:page]||1)
-                                        }])
+        data = [{
+                    tag: params[:tag],
+                    item: News.where(tag: params[:tag]).order(id: :desc).page(params[:page]||1)
+                }]
       else
-        render json: Success.new(news: News::TAG.map { |tag|
-                                   {
-                                       tag: tag,
-                                       item: News.where(tag: tag).order(id: :desc).take(2)
-                                   }
-                                 })
+        data = News::TAG.map { |tag|
+          {
+              tag: tag,
+              item: News.where(tag: tag).order(id: :desc).take(2)
+          } if News.where(tag: tag).present?
+        }
       end
+      data.delete(nil)
+      render json: Success.new(news: data)
     end
   end
 end
