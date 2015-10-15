4b805be3937dc1b5ff4fa2bec0f993cfa9e88a50
root <root@localhost.localdomain>
2015-10-15 15:58:30 +0800

    migration

--- app/models/concerns/profile_able.rb
+++ app/models/concerns/profile_able.rb
@@ -1,28 +1,28 @@
 module ProfileAble
   extend ActiveSupport::Concern
-  # included do
-  #   after_create :regist_to_easemob
-  #   after_destroy :delete_from_easemob
-  # end
-  # private
-  # def regist_to_easemob
-  #   easemob_token = Rails.cache.fetch('mob')
-  #   Faraday.post do |req|
-  #     req.url "#{MOB['host']}/users"
-  #     req.headers['Content-Type'] = 'application/json'
-  #     req.headers['Authorization'] = "Bearer #{easemob_token}"
-  #     req.body = "{\"username\": \"#{mxid}\", \"password\": \"123456\", \"nickname\": \"#{name}\"}"
-  #   end
-  #   MessageJob.set(wait: 1.minute).perform_later(id, MESSAGE['欢迎语'])
-  # end
-  #
-  # def delete_from_easemob
-  #   easemob_token = Rails.cache.fetch('mob')
-  #   Faraday.delete do |req|
-  #     req.url "#{MOB['host']}/users/#{mxid}"
-  #     req.headers['Content-Type'] = 'application/json'
-  #     req.headers['Authorization'] = "Bearer #{easemob_token}"
-  #     req.body = ''
-  #   end
-  # end
+  included do
+    after_create :regist_to_easemob
+    after_destroy :delete_from_easemob
+  end
+  private
+  def regist_to_easemob
+    easemob_token = Rails.cache.fetch('mob')
+    Faraday.post do |req|
+      req.url "#{MOB['host']}/users"
+      req.headers['Content-Type'] = 'application/json'
+      req.headers['Authorization'] = "Bearer #{easemob_token}"
+      req.body = "{\"username\": \"#{mxid}\", \"password\": \"123456\", \"nickname\": \"#{name}\"}"
+    end
+    MessageJob.set(wait: 1.minute).perform_later(id, MESSAGE['欢迎语'])
+  end
+
+  def delete_from_easemob
+    easemob_token = Rails.cache.fetch('mob')
+    Faraday.delete do |req|
+      req.url "#{MOB['host']}/users/#{mxid}"
+      req.headers['Content-Type'] = 'application/json'
+      req.headers['Authorization'] = "Bearer #{easemob_token}"
+      req.body = ''
+    end
+  end
 end
