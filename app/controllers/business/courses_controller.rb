235498fe6af893063c2611881031356853d6c04c
root <root@localhost.localdomain>
2015-10-15 14:42:44 +0800

    migrate

--- app/controllers/business/courses_controller.rb
+++ app/controllers/business/courses_controller.rb
@@ -9,7 +9,7 @@ module Business
                        type: sku.course_type,
                        style: sku.course.style,
                        during: sku.course.during,
-                       price: sku.course.selling_price,
+                       price: sku.selling_price,
                        exp: sku.course.exp,
                        proposal: sku.course.proposal,
                        intro: sku.course.intro,
