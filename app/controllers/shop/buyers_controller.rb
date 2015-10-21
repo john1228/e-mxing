888afb9759cb3d3ecd04c3fac7c2ca21de2bc016
root <root@localhost.localdomain>
2015-10-21 20:21:14 +0800

    update

--- app/controllers/shop/buyers_controller.rb
+++ app/controllers/shop/buyers_controller.rb
@@ -3,7 +3,7 @@ module Shop
     #私教评论列表
     def index
       render json: Success.new(buyer: Order.joins(:order_item).where(status: 2).where('order_items.sku LIKE ?', params[:sku][0, params[:sku].rindex('-')] + '%').
-                                   order(id: :desc).pluck(:user_id).page(params[:page]||1).map { |user_id|
+                                   order(id: :desc).page(params[:page]||1).pluck(:user_id).map { |user_id|
                                  user = User.find(user_id)
                                  user.summary_json
                                })
