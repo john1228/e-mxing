3e5e3c88f467cf6d1e423eaa9b3a30f1cd9508f6
root <root@localhost.localdomain>
2015-10-29 16:02:16 +0800

    change carrierwave assets host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,14 @@
 ::CarrierWave.configure do |config|
-  config.storage             = :qiniu
-  config.qiniu_access_key    = '51PeOMLBoxi1VFBrZgT8vTe-mW64g19DbEtPUCem'
-  config.qiniu_secret_key    = 'IlQDXWZROSw8shqT42s5nw4Bp2ol-BmyOsjd21g9'
-  config.qiniu_bucket        = 'emxing'
+  config.storage = :qiniu
+  config.qiniu_access_key = '51PeOMLBoxi1VFBrZgT8vTe-mW64g19DbEtPUCem'
+  config.qiniu_secret_key = 'IlQDXWZROSw8shqT42s5nw4Bp2ol-BmyOsjd21g9'
+  config.qiniu_bucket = 'emxing'
   config.qiniu_bucket_domain = '7xl2f5.com1.z0.glb.clouddn.com'
-  config.qiniu_block_size    = 4*1024*1024
+  config.qiniu_block_size = 4*1024*1024
+  config.asset_host = 'http://stage.e-mxing.com'
   #config.qiniu_bucket_private= true #default is false
   #config.qiniu_protocol      = "http"
   #config.qiniu_up_host       = 'http://up.qiniug.com' #七牛上传海外服务器,国内使用可以不要这行配置
+  config.asset_host = 'http://7xl2f5.com1.z0.glb.clouddn.com'
 end
 
