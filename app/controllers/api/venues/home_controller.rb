4efd5b2d10d7a8bb02fc29ba657eb5a50179e859
root <root@localhost.localdomain>
2015-11-03 18:21:44 +0800

    fix v33 bug

--- app/controllers/api/venues/home_controller.rb
+++ app/controllers/api/venues/home_controller.rb
@@ -9,7 +9,7 @@ module Api
                                    {
                                        mxid: venue.profile.mxid,
                                        name: venue.profile.name,
-                                       avatar: venue.profile.avatar,
+                                       avatar: venue.profile.avatar.url,
                                        background: (venue.photos.first.photo.url rescue ''),
                                        address: venue.profile.province.to_s + venue.profile.city.to_s + venue.profile.address.to_s,
                                        distance: venue.attributes['distance'].to_i,
@@ -32,7 +32,20 @@ module Api
             where('profiles.name LIKE ? or profiles.address LIKE ?', "%#{params[:keyword]||''}%", "%#{params[:keyword]||''}%")
         venues.where('? = ANY (profiles.tag_1)', params[:tag]) if params[:tag].present?
         venues.where(profiles: [auth: params[:auth]]) if params[:auth].present?
-        render json: Success.new(venues: venues.order('distance asc').order(id: :desc).page(params[:page]||1))
+        render json: Success.new(venues: venues.order('distance asc').order(id: :desc).page(params[:page]||1).map { |venue|
+                                   {
+                                       mxid: venue.profile.mxid,
+                                       name: venue.profile.name,
+                                       avatar: venue.profile.avatar.url,
+                                       background: (venue.photos.first.photo.url rescue ''),
+                                       address: venue.profile.province.to_s + venue.profile.city.to_s + venue.profile.address.to_s,
+                                       distance: venue.attributes['distance'].to_i,
+                                       coach_count: venue.coaches.count,
+                                       sale: venue.courses.online.count,
+                                       tag: venue.profile.tag,
+                                       auth: venue.profile.auth
+                                   }
+                                 })
       end
     end
   end
