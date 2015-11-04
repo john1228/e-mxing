4d54eff8203313c75590fdcd568c3352300c9d32
root <root@localhost.localdomain>
2015-11-04 16:11:20 +0800

    fix venues search bug

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -19,7 +19,21 @@ module Api
       case params[:cat]
         when 'venues'
           venues = search_venues(keyword, params[:page]||1)
-          render json: Success.new(venues: venues)
+          render json: Success.new(venues: venues.map { |venue|
+                                     {
+                                         mxid: venue.profile.mxid,
+                                         name: venue.profile.name,
+                                         avatar: venue.profile.avatar.url,
+                                         background: (venue.photos.first.photo.url rescue ''),
+                                         address: venue.profile.province.to_s + venue.profile.city.to_s + venue.profile.address.to_s,
+                                         distance: venue.attributes['distance'].to_i,
+                                         coach_count: venue.coaches.count,
+                                         sale: venue.courses.online.count,
+                                         tag: venue.profile.tag,
+                                         auth: venue.profile.auth,
+                                         floor: (venue.courses.online.order(selling_price: :asc).first.selling_price rescue ''),
+                                     }
+                                   })
         when 'course'
           courses = search_course(keyword, params[:page]||1)
           render json: Success.new(course: courses)
