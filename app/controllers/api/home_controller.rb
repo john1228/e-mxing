204a5c0576acf3c394f951f03841f5422fcc969f
root <root@localhost.localdomain>
2015-11-04 16:05:43 +0800

    change dynamic api

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -32,7 +32,21 @@ module Api
         else
           venues = search_venues(keyword, params[:page]||1)
           if venues.present?
-            render json: Success.new(venues: venues)
+            render json: Success.new(venues: venues.map { |venue|
+                                       {
+                                           mxid: venue.profile.mxid,
+                                           name: venue.profile.name,
+                                           avatar: venue.profile.avatar.url,
+                                           background: (venue.photos.first.photo.url rescue ''),
+                                           address: venue.profile.province.to_s + venue.profile.city.to_s + venue.profile.address.to_s,
+                                           distance: venue.attributes['distance'].to_i,
+                                           coach_count: venue.coaches.count,
+                                           sale: venue.courses.online.count,
+                                           tag: venue.profile.tag,
+                                           auth: venue.profile.auth,
+                                           floor: (venue.courses.online.order(selling_price: :asc).first.selling_price rescue ''),
+                                       }
+                                     })
           else
             courses = search_course(keyword, params[:page]||1)
             if courses.present?
