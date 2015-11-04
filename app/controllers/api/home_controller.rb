1a02be7223f66d26931619be60b45d0d16f93aa3
root <root@localhost.localdomain>
2015-11-04 16:43:34 +0800

    change key of home

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -6,7 +6,7 @@ module Api
                    online_courses = Sku.online.where(course_type: category.item).order(selling_price: :asc)
                    {
                        tag: category.name,
-                       backgournd: category.background.url,
+                       bg: category.background.url,
                        amount: online_courses.count,
                        lowest: online_courses.first.selling_price
                    }
