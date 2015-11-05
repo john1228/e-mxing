d873b528795afd9dfdeccbc138f0ce4ce07bc51e
root <root@localhost.localdomain>
2015-11-05 16:09:00 +0800

    change json key background to bg

--- app/controllers/api/recommend_controller.rb
+++ app/controllers/api/recommend_controller.rb
@@ -69,7 +69,7 @@ module Api
                                      mxid: venue.profile.mxid,
                                      name: venue.profile.name,
                                      avatar: venue.profile.avatar.url,
-                                     background: (venue.photos.first.photo.url rescue ''),
+                                     bg: (venue.photos.first.photo.url rescue ''),
                                      address: venue.profile.province.to_s + venue.profile.city.to_s + venue.profile.address.to_s,
                                      distance: venue.attributes['distance'].to_i,
                                      coach_count: venue.coaches.count,
