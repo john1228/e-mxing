652de9836987e6c98742a5b111e291840d20f77c
root <root@localhost.localdomain>
2015-11-07 15:21:18 +0800

    add comment for strategy

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,31 @@
 class News < ActiveRecord::Base
   enum tag: {knowledge: '健身知识', question: '你问我答', venues: '热门场馆', gyms: '金牌私教'}
   mount_uploader :cover, NewsCoverUploader
+  
+  class<<self
+    def top_news(category)
+      [{
+           name: '场馆推荐',
+           tip: (category_news(category, tags[:venues]).first.title rescue '')
+       },
+       {
+           name: '私教专访',
+           tip: (category_news(category, tags[:gyms]).first.title rescue '')
+       },
+       {
+           name: "玩转#{category}",
+           tip: (category_news(category, tags[:knowledge]).first.title rescue '')
+       },
+       {
+           name: '玩家攻略',
+           tip: "大家聊#{category}"
+       }]
+    end
+
+    def category_news(category, sub_category)
+      where('? = ANY(tag_1) AND tag = ?', category, sub_category).order(id: :desc)
+    end
+  end
 
   def as_json
     {
@@ -12,29 +37,5 @@ class News < ActiveRecord::Base
         url: $host + "/news/#{id}",
         created_at: created_at.to_i
     }
-  end
-
-  def top_news(category)
-    [{
-         name: '场馆推荐',
-         tip: (News.category_news(category, tags[:venues]).first.title rescue '')
-     },
-     {
-         name: '私教专访',
-         tip: (News.category_news(category, tags[:gyms]).first.title rescue '')
-     },
-     {
-         name: "玩转#{category}",
-         tip: (News.category_news(category, tags[:knowledge]).first.title rescue '')
-     },
-     {
-         name: '玩家攻略',
-         tip: "大家聊#{category}"
-     }]
-  end
-
-
-  def category_news(category, sub_category)
-    where('? = ANY(tag_1) AND tag = ?', category, sub_category).order(id: :desc)
   end
 end
