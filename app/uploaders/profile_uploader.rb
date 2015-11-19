1378802b99ba519f615c474d440d704f140d2e32
root <313423716@qq.com>
2015-11-19 15:58:16 +0800

    fix sns login bug

--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -8,4 +8,19 @@ class ProfileUploader < CarrierWave::Uploader::Base
   def store_dir
     "images/#{model.class.to_s.underscore}"
   end
+
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  def filename
+    "#{Time.now.strftime('%Y/%m/%d')}/#{secure_token}.#{file.extension}" if original_filename
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
