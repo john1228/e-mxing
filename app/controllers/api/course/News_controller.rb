694cdaf6028a1b99d99659ef09433cdf79a8a555
root <313423716@qq.com>
2015-11-20 18:08:35 +0800

    fix popup bug

--- app/controllers/api/course/News_controller.rb
+++ app/controllers/api/course/News_controller.rb
@@ -4,7 +4,7 @@ module Api
       #私教评论列表
       def index
         render json: Success.new(
-                   news: News.category_news(params[:category], sub_category)
+                   news: News.category_news(params[:category], params[:sub_category])
                )
       end
 
