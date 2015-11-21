4dfa447344c6be02469e7bd80e6199c1ef1ee20d
root <313423716@qq.com>
2015-11-21 16:20:38 +0800

    fix bug

--- app/controllers/api/course/News_controller.rb
+++ app/controllers/api/course/News_controller.rb
@@ -4,7 +4,7 @@ module Api
       #私教评论列表
       def index
         render json: Success.new(
-                   news: News.category_news(params[:category], params[:sub_category])
+                   news: News.category_news(params[:category], params[:sub_category]).page(params[:page]||1)
                )
       end
 
