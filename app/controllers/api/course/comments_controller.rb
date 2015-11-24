7c6d5bddc30248e4ad9a0c2d9cc52af67e0ecc7a
root <313423716@qq.com>
2015-11-24 16:15:31 +0800

    change image json

--- app/controllers/api/course/comments_controller.rb
+++ app/controllers/api/course/comments_controller.rb
@@ -5,39 +5,39 @@ module Api
       def index
         sku = Sku.find_by(sku: params[:sku])
         if params[:page].eql?('1')
-          render json: Success.new(count: sku.comments.count, comments: sku.comments.page(params[:page]||1)).map { |comment|
-                   {
-                       user: {
-                           mxid: comment.user.profile.mxid,
-                           name: comment.user.profile.name,
-                           avatar: comment.user.profile.avatar.url,
-                           age: comment.user.profile.age,
-                           gender: comment.user.profile.gender.to_i,
-                           identity: comment.user.profile.identity_value,
-                       },
-                       score: comment.score,
-                       content: comment.content,
-                       images: comment.image.map { |image| {url: image.url} },
-                       created: comment.created_at.localtime.strftime('%Y-%m-%d %H:%M:%S')
-                   }
-                 }
+          render json: Success.new(count: sku.comments.count, comments: sku.comments.page(params[:page]||1).map { |comment|
+                                                              {
+                                                                  user: {
+                                                                      mxid: comment.user.profile.mxid,
+                                                                      name: comment.user.profile.name,
+                                                                      avatar: comment.user.profile.avatar.url,
+                                                                      age: comment.user.profile.age,
+                                                                      gender: comment.user.profile.gender.to_i,
+                                                                      identity: comment.user.profile.identity_value,
+                                                                  },
+                                                                  score: comment.score,
+                                                                  content: comment.content,
+                                                                  images: comment.image.map { |image| {url: image.url} },
+                                                                  created: comment.created_at.localtime.strftime('%Y-%m-%d %H:%M:%S')
+                                                              }
+                                                            })
         else
-          render json: Success.new(comments: sku.comments.page(params[:page]||1)).map { |comment|
-                   {
-                       user: {
-                           mxid: comment.user.profile.mxid,
-                           name: comment.user.profile.name,
-                           avatar: comment.user.profile.avatar.url,
-                           age: comment.user.profile.age,
-                           gender: comment.user.profile.gender.to_i,
-                           identity: comment.user.profile.identity_value,
-                       },
-                       score: comment.score,
-                       content: comment.content,
-                       images: comment.image.map { |image| image.url },
-                       created: comment.created_at.localtime.strftime('%Y-%m-%d %H:%M:%S')
-                   }
-                 }
+          render json: Success.new(comments: sku.comments.page(params[:page]||1).map { |comment|
+                                     {
+                                         user: {
+                                             mxid: comment.user.profile.mxid,
+                                             name: comment.user.profile.name,
+                                             avatar: comment.user.profile.avatar.url,
+                                             age: comment.user.profile.age,
+                                             gender: comment.user.profile.gender.to_i,
+                                             identity: comment.user.profile.identity_value,
+                                         },
+                                         score: comment.score,
+                                         content: comment.content,
+                                         images: comment.image.map { |image| image.url },
+                                         created: comment.created_at.localtime.strftime('%Y-%m-%d %H:%M:%S')
+                                     }
+                                   })
         end
       end
     end
--- app/controllers/api/gyms/comments_controller.rb
+++ app/controllers/api/gyms/comments_controller.rb
@@ -3,22 +3,22 @@ module Api
     class CommentsController < BaseController
       #私教评论列表
       def index
-        render json: Success.new(comment: @coach.comments.page(params[:page]||1)).map { |comment|
-                 {
-                     user: {
-                         mxid: comment.user.profile.mxid,
-                         name: comment.user.profile.name,
-                         avatar: comment.user.profile.avatar.url,
-                         age: comment.user.profile.age,
-                         gender: comment.user.profile.gender.to_i,
-                         identity: comment.user.profile.identity_value,
-                     },
-                     score: comment.score,
-                     content: comment.content,
-                     images: comment.image.map { |image| {url: image.url} },
-                     created: comment.created_at.localtime.strftime('%Y-%m-%d %H:%M:%S')
-                 }
-               }
+        render json: Success.new(comment: @coach.comments.page(params[:page]||1).map { |comment|
+                                   {
+                                       user: {
+                                           mxid: comment.user.profile.mxid,
+                                           name: comment.user.profile.name,
+                                           avatar: comment.user.profile.avatar.url,
+                                           age: comment.user.profile.age,
+                                           gender: comment.user.profile.gender.to_i,
+                                           identity: comment.user.profile.identity_value,
+                                       },
+                                       score: comment.score,
+                                       content: comment.content,
+                                       images: comment.image.map { |image| {url: image.url} },
+                                       created: comment.created_at.localtime.strftime('%Y-%m-%d %H:%M:%S')
+                                   }
+                                 })
       end
     end
   end
