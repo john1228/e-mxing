474f2f6c05652a8a5faa672f48235ad595e25d39
root <313423716@qq.com>
2015-11-27 22:47:01 +0800

    fix business bug

--- app/controllers/business/base_controller.rb
+++ app/controllers/business/base_controller.rb
@@ -3,7 +3,7 @@ module Business
     before_action :verify_auth_token, except: :mobile
     private
     def verify_auth_token
-      @coach = Rails.cache.fetch("gyms-#{coach.token}")
+      @coach = Rails.cache.fetch("gyms-#{request.headers[:token]}")
       render json: {code: -1, message: '您还为登录'} if @coach.blank?
     end
   end
