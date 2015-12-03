5a91400542de2fabe11475b3c4368a1c1548d581
root <313423716@qq.com>
2015-12-03 19:14:13 +0800

    fix bug

--- app/controllers/business/base_controller.rb
+++ app/controllers/business/base_controller.rb
@@ -3,7 +3,6 @@ module Business
     before_filter :verify_auth_token
     private
     def verify_auth_token
-      logger.info "私教登录TOKEN：#{request.headers[:token]}"
       @coach = Rails.cache.fetch("gyms-#{request.headers[:token]}")
       render json: {code: -1, message: '您还为登录'} if @coach.blank?
     end
--- app/controllers/business/students/courses_controller.rb
+++ app/controllers/business/students/courses_controller.rb
@@ -1,22 +1,24 @@
 module Business
-  class CoursesController < BaseController
-    def index
-      user = User.find_by_mxid(params[:student])
-      if user.blank?
-        render json: Failure.new('您查看到用户不存在')
-      else
-        render json: Success.new(
-                   courses: @coach.lessons.where('lessons.available > lessons.used and lessons.user_id = ?', user.id).order(exp: :asc).map { |lesson|
-                     {
-                         id: lesson.id,
-                         course: lesson.course.course_name,
-                         student: lesson.user.profile.name,
-                         seller: lesson.course.seller,
-                         available: lesson.available,
-                         used: lesson.appointments.pluck(:code)
+  module Students
+    class CoursesController < BaseController
+      def index
+        user = User.find_by_mxid(params[:student])
+        if user.blank?
+          render json: Failure.new('您查看到用户不存在')
+        else
+          render json: Success.new(
+                     courses: @coach.lessons.where('lessons.available > lessons.used and lessons.user_id = ?', user.id).order(exp: :asc).map { |lesson|
+                       {
+                           id: lesson.id,
+                           course: lesson.course.course_name,
+                           student: lesson.user.profile.name,
+                           seller: lesson.course.seller,
+                           available: lesson.available,
+                           used: lesson.appointments.pluck(:code)
+                       }
                      }
-                   }
-               )
+                 )
+        end
       end
     end
   end
