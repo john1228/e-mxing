38e4740bb618f19a268217374edb3f2ee9b18306
root <313423716@qq.com>
2015-12-10 18:04:52 +0800

    change routes

--- app/controllers/business/home_controler.rb
+++ app/controllers/business/home_controler.rb
@@ -6,7 +6,7 @@ module Business
                  balance: @coach.wallet.balance.to_f.round(2),
                  order: Order.pay.where(coach_id: @coach.id).count,
                  appoint: Appointment.where(coach_id: @coach.id).count,
-                 comment: Comment.where(sku: Sku.where(seller_id: @coach.id).pluck(:sku)).count
+                 comment: Comment.where(sku: Sku.where(seller_id: @coach.id).pluck(:sku).uniq).count
              )
     end
 
