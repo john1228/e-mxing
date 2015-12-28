27b4a1c23f5ef7ea77a589d13095322b31c3b27b
root <313423716@qq.com>
2015-12-28 17:13:36 +0800

    fix bug

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,6 +7,7 @@ module Api
       if order.blank?
         render json: Failure.new('您查看到订单不存在')
       else
+        seller = Sku.find(order.order_item.sku)
         render json: Success.new(
                    order: {
                        no: order.no,
