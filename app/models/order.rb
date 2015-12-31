446914e673d74e54dbc496118b507cf2c16a79a1
root <313423716@qq.com>
2015-12-31 20:33:44 +0800

    fix bug

--- app/models/order.rb
+++ app/models/order.rb
@@ -215,28 +215,6 @@ class Order < ActiveRecord::Base
                 )
               end
             end
-
-
-            def create
-              @membership_card = MembershipCard.new(membership_card_params)
-              @membership_card.logs.build(membership_card_log_params)
-              if @membership_card.save
-                redirect_to membership_cards_path
-              else
-                render action: :new
-              end
-            end
-
-            protected
-            def membership_card_params
-              params.require(:membership_card).permit(:card_type, :service_id, :member_id, :name, :value, :valid_start, :valid_end, :physical_card)
-            end
-
-            def membership_card_log_params
-              params.permit(:market_price, :selling_price, :pay_type, :seller, :remark)
-            end
-
-
           end
         when STATUS[:cancel]
           transaction do
