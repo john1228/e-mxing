07d5bc5250ec15dd9db0114955697dfb14e79ae1
root <313423716@qq.com>
2016-01-03 16:35:23 +0800

    fix bug

--- app/controllers/mine/orders_controller.rb
+++ app/controllers/mine/orders_controller.rb
@@ -54,7 +54,7 @@ module Mine
                                 during: order.order_item.during,
                                 price: order.order_item.price,
                                 amount: order.order_item.amount,
-                                card: sku.product.present? ? 1 : 0,
+                                card: order.order_item.course.product.present? ? 1 : 0,
                                 card_type: (sku.product.card_type.card_type rescue '')}],
                        contact: {
                            name: order.contact_name,
