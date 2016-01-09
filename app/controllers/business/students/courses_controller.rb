9a152f840d3d34063e2a4066bd9581fe6f4b3320
root <313423716@qq.com>
2016-01-09 16:33:43 +0800

    fix bug

--- app/controllers/business/students/courses_controller.rb
+++ app/controllers/business/students/courses_controller.rb
@@ -7,14 +7,14 @@ module Business
           render json: Failure.new('您查看到用户不存在')
         else
           render json: Success.new(
-                     courses: @coach.lessons.where('lessons.available > lessons.used and lessons.user_id = ?', user.id).order(exp: :asc).map { |lesson|
+                     courses: MembershipCard.includes(:order).where(orders: {coach_id: @coach.id}).where('supply_value > 0').map { |membership_card|
                        {
-                           id: lesson.id,
-                           course: lesson.course.course_name,
-                           cover: lesson.course.course_cover,
-                           during: lesson.course.course_during,
-                           available: lesson.available,
-                           used: lesson.used
+                           id: membership_card.id,
+                           course: membership_card.name,
+                           cover: membership_card.order.order_item.cover,
+                           during: membership_card.order.order_item.during,
+                           available: membership_card.supply_value,
+                           used: 0
                        }
                      }
                  )
