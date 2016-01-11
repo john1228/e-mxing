ef583444051004f27c18decb1dc0bf13536ec0e2
root <313423716@qq.com>
2016-01-11 15:30:44 +0800

    fix bug

--- app/models/membership_card_type.rb
+++ app/models/membership_card_type.rb
@@ -1,7 +1,7 @@
 class MembershipCardType < ActiveRecord::Base
   enum card_type: [:stored, :measured, :clocked, :course]
   belongs_to :service
-  has_many :products, dependent: :destroy, foreign_key: :card_type
+  has_many :products, dependent: :destroy, foreign_key: :card_type_id
 
   def card_type_value
     MembershipCardType.card_types[card_type]
